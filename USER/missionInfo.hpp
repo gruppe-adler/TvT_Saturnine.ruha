a1263e36e6ade7ce814914138e1a5abecd62baf2
nomisum <nomisum@googlemail.com>
2019-09-06 18:18:55 +0200

    add intro song, loading pic

--- USER/missionInfo.hpp
+++ USER/missionInfo.hpp
@@ -5,4 +5,4 @@
 author = "nomisum for Gruppe Adler";                                               //Missionsersteller (Du)
 onLoadName = "Seraphine";                                                    //Name der Mission
 onLoadMission = "";                                                             //Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
-loadScreen = "data\loadpic.paa";                                                //Ladebild
+loadScreen = "data\loading.jpg";                                                //Ladebild

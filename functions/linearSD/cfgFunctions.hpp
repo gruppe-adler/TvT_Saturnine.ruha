2648d8454ca6caa8b8ccc7352504d33f5a304676
nomisum <nomisum@googlemail.com>
2019-09-06 17:21:52 +0200

    Initial commit

---
+++ functions/linearSD/cfgFunctions.hpp
@@ -0,0 +1,32 @@
+class grad_linearSD {
+    class linearSD {
+        class activateFortifications {};
+        class activeSectorBeingCaptured {};
+        class addChatCommands {};
+        class addFortifications {};
+        class addRespawnPosition {};
+        class addTeleportAction {};
+        class addVehicles {};
+        class createSector {};
+        class dynamicText {};
+        class endRound {};
+        class getWeather {};
+        class isLastSector {};
+        class movePlayerToRespawnPos {};
+        class moveRespawnPositions {};
+        class onPlayerKilled {};
+        class onSectorCaptured {};
+        class playzoneCleanup {};
+        class respawnPlayer {};
+        class setActiveSectors {};
+        class setLastRound {};
+        class setTimeAndDate {};
+        class setWeather {};
+        class spawnSectorVehicles {};
+        class startEliminationCheck {};
+        class startLinearSD {};
+        class startNewRound {};
+        class startTimeout {};
+        class transferRadiosAcrossRespawn {};
+    };
+};
